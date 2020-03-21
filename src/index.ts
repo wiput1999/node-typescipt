3bebc77ab14acc91ffc14dfce9c5a0b3707bc368
Wiput Pootong <wiput.pootong@gmail.com>
2020-03-22 03:18:56 +0700

    :sparkles: add typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
 import express from 'express'
+import { Request, Response } from 'express'
 
-const app = express()
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { Posts } from './entity/Posts'
 
-const PORT = 5000 || process.env.PORT
+createConnection({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '',
+  database: 'node_typescript',
+  entities: ['src/entity/*.ts'],
+  migrations: ['src/migration/*.ts']
+}).then(connection => {
+  const app = express()
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
+  const postRepository = connection.getRepository(Posts)
+
+  app.get('/posts', async function(_: Request, res: Response) {
+    const posts = await postRepository.find()
+    res.json(posts)
+  })
+
+  const PORT = 5000 || process.env.PORT
+
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
+})
---
+++ src/migration/1584821170144-Post.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class Post1584821170144 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'posts',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true
+          },
+          {
+            name: 'title',
+            type: 'text'
+          },
+          {
+            name: 'text',
+            type: 'text'
+          }
+        ]
+      }),
+      true
+    )
+  }
+
+  async down(_: QueryRunner): Promise<void> {}
+}
